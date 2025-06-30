2fb27d6c1118f9f135abce20405254e25d4e5865
Sasho Stojkoski <112008646+Salewitcher@users.noreply.github.com>
2025-06-30 16:23:01 +0100

    Configure CORS to allow frontend and localhost

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,21 @@ const nasaRoutes = require('./routes/nasaRoutes');
 
 const app = express();
 
-app.use(cors());
+const allowedOrigins = [
+  'https://nasa-frontend-kappa.vercel.app',
+  'http://localhost:3000'
+];
+
+app.use(cors({
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true); // allow requests like Postman
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 // Root test route
 app.get('/', (req, res) => {
